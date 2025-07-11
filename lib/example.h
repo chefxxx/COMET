3c5147bbdb3aa73cc335f16a0bf201ee74ae775a
chefxxx <mateusz.mm100@gmail.com>
2025-07-11 15:26:34 +0200

    Initial project setup

---
+++ lib/example.h
@@ -0,0 +1,10 @@
+//
+// Created by Mateusz Mikiciuk on 11/07/2025.
+//
+
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+struct Example {};
+
+#endif //EXAMPLE_H
---
+++ tests/example.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Mateusz Mikiciuk on 11/07/2025.
+//
+
+#include <gtest/gtest.h>
+#include "example.h"
+
+TEST(Example, Example) {
+    ASSERT_EQ(1, 1);
+    ASSERT_TRUE(true);
+}
