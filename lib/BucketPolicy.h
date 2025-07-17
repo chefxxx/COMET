0e076bde2662d72d9e016c36021b026c4c2b7f9c
chefxxx <mateusz.mm100@gmail.com>
2025-07-17 15:19:11 +0200

    Added BucketPolicy prototype struct

--- lib/BucketPolicy.h
+++ lib/BucketPolicy.h
@@ -5,8 +5,19 @@
 #ifndef BUCKETPOLICY_H
 #define BUCKETPOLICY_H
 
-struct BucketPolicy {
+#include <string>
 
+template <typename... TCallables>
+struct BucketPolicy {
+    BucketPolicy(std::tuple<TCallables...> callables, std::array<std::vector<double>, sizeof...(TCallables)> bucketsRanges) {}
+
+private:
+    std::tuple<TCallables...> mCallables;
+    std::array<std::vector<double>, sizeof...(TCallables)> mBucketsRanges;
+
+    auto getBucket();
+    auto getBucketRangeVal();
 };
+
 
 #endif //BUCKETPOLICY_H
