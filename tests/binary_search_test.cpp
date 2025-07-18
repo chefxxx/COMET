e289c5a379c7c770f86eb9eb66ad3161618155d4
MykhailoShamrai <mike.shamrai@gmail.com>
2025-07-18 16:27:27 +0200

    Some tests for finding ranges

---
+++ tests/binary_search_test.cpp
@@ -0,0 +1,47 @@
+//
+// Created by Mykhailo Shamrai on 18/07/2025.
+//
+
+#include <gtest/gtest.h>
+#include <Helpers.h>
+
+TEST(BinarySearchTest, oneElementVector_indexOne)
+{
+    std::vector<double> v{0.0};
+
+    double value = 1.0;
+
+    int res = 1;
+    int test = findUpperIndex(v, value);
+    ASSERT_EQ(res, test);
+}
+
+TEST(BinarySearchTest, emptyVector) {
+    std::vector<double> v;
+
+    double value = 1.0;
+
+    int res = -1;
+    int test = findUpperIndex(v, value);
+    ASSERT_EQ(res, test);
+}
+
+TEST(BinarySearchTest, testCase0_4) {
+    std::vector<double> v{0.0, 1.0, 2.0, 3.0, 4.0};
+
+    double value = 3.6;
+    int res = 4;
+    int test = findUpperIndex(v, value);
+
+    ASSERT_EQ(res, test);
+}
+
+TEST(BinarySearchTest, testCaseUnderflow_0) {
+    std::vector<double> v{-1.0, 0.0, 1.0, 2.0, 3.0, 4.0};
+
+    double value = -313.0;
+    int res = 0;
+    int test = findUpperIndex(v, value);
+
+    ASSERT_EQ(res, test);
+}
