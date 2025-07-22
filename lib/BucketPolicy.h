0a01c11192500fa4e31525d7ccc02698cc65005d
MykhailoShamrai <mike.shamrai@gmail.com>
2025-07-22 11:36:39 +0200

    Changed getBucket return value to int and added method for finding index of bucket from tuple of upperIndices

--- lib/BucketPolicy.h
+++ lib/BucketPolicy.h
@@ -6,8 +6,9 @@
 #define BUCKETPOLICY_H
 
 #include <array>
-#include <utility>
+#include <complex>
 #include <tuple>
+#include <utility>
 #include <vector>
 
 template <typename T>
@@ -39,21 +40,33 @@ struct BucketPolicy final {
     template<typename TElement>
         requires is_function_binable_on_doubles<TElement, TCallables...>
     [[nodiscard]] auto getBucket(TElement const &arg) {
-        auto values = std::make_tuple(std::get<TCallables>(mCallables)(arg)...);
+        return calculateBucketAtIndices(getUpperIndicesForTuple(getValues(arg)));
+    }
+
+private:
+    template<typename TElement>
+    auto getValues(TElement const &arg) {
+        return std::make_tuple(std::get<TCallables>(mCallables)(arg)...);
+    }
+
+    FRIEND_TEST(BinarySearchTest, testOfGetBucket_ReturnsAtupleOfIndices);
+    template<typename... Ts>
+    auto getUpperIndicesForTuple(std::tuple<Ts...> const& values) {
         return [&]<std::size_t... I>(std::index_sequence<I...>)
             {
                 return std::make_tuple(findUpperIndex(std::get<I>(mBucketsRanges), std::get<I>(values))...);
             }(std::make_index_sequence<sizeof...(TCallables)>{});
     }
 
-private:
+    FRIEND_TEST(GetBucketAtTest, test1);
     template<typename... TIndices>
     int calculateBucketAtIndices(std::tuple<TIndices...> const& indices) {
-        constexpr int N = sizeof...(TIndices);
-        auto indexSeq = std::make_index_sequence<N>();
-        auto res = [&]<size_t... I, size_t... S>(std::index_sequence<I...> first, std::index_sequence<S...> second)
+        constexpr auto N = sizeof...(TIndices);
+        auto indexSeq = std::make_index_sequence<N - 1>();
+        return  [&]<size_t... I>(std::index_sequence<I...> first)
         {
-        };
+            return (std::get<0>(indices) + ... + (std::get<I + 1>(indices) * (1 * ...  * (std::get<I>(mBucketsRanges).size() + 1))));
+        }(indexSeq);
     }
 
     std::tuple<TCallables...> mCallables;
