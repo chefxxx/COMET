3574b09c31fa322b7dd9b0b0368ebd4c7bfe76be
chefxxx <mateusz.mm100@gmail.com>
2025-07-23 14:27:11 +0200

    Added actions on pull_request

--- lib/Combinations.h
+++ lib/Combinations.h
@@ -8,14 +8,7 @@
 #include <iostream>
 
 struct FullCombinations {
-    int P = 10;
 
-    FullCombinations(int P_) : P(P_) {}
-
-    void func(int xd)
-    {
-        for (int i = 1; i <= P; i++) std::cout << i << '\n';
-    }
 };
 
 struct StrictlyUpperCombinations {
