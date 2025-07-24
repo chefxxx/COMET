a974d100781db5524f16133a21331f3d229c831c
chefxxx <mateusz.mm100@gmail.com>
2025-07-24 12:50:33 +0200

    Add initial bucket count test

--- tests/groupData_test.cpp
+++ tests/groupData_test.cpp
@@ -8,19 +8,14 @@
 #include "BucketPolicy.h"
 #include "Helpers.h"
 
-namespace testStuff
+namespace groupDataTest
 {
 struct MyPoint {
     double x;
     double y;
 };
 
-std::ostream &operator<<(std::ostream &os, const MyPoint &p)
-{
-    os << "(" << p.x << ", " << p.y << ")";
-    return os;
-}
-
+/* lambdas defined for testing */
 auto lambda1 = [](const MyPoint &a) {
     return a.x;
 };
@@ -30,9 +25,14 @@ auto lambda2 = [](const MyPoint &b) {
 
 const auto callables = std::make_tuple(lambda1, lambda2);
 
+/* vectors of bins */
 const std::vector vec1{0.0, 0.5, 1.0};
 const std::vector vec2{0.0, 0.5, 1.0};
 
+const std::vector vec3{0.0, 0.5, 1.0, 1.5, 2.0};
+const std::vector vec4{0.0, 0.5, 1.0, 1.5, 2.0};
+
+/* Elements that are subject of binning */
 MyPoint p1{-0.1, 0.1};
 MyPoint p2{-0.1, -0.1};
 MyPoint p3{-0.1, 0.6};
@@ -51,23 +51,16 @@ MyPoint p15{1.6, 0.6};
 MyPoint p16{1.6, 1.1};
 
 const std::vector points{p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16};
+
 }  // namespace testStuff
 
-TEST(LambdasTest, doesBpCompile)
-{
-    auto bp = BucketPolicy(testStuff::callables, {testStuff::vec1, testStuff::vec2});
-}
 
-TEST(GroupTableTest, doesGroupDataGroups)
+TEST(GroupDataTest, initialBucketCount)
 {
-    const auto bp = BucketPolicy(testStuff::callables, {testStuff::vec1, testStuff::vec2}, false);
-    const auto bucketCount = bp.getInitialBucketCount();
-    ASSERT_EQ(bucketCount, 16);
-    std::cout << "Initial bucket count: " << bucketCount << std::endl;
-    const auto res = groupData(testStuff::points.begin(), testStuff::points.end(), bp);
-    ASSERT_EQ(res.size(), 16);
-    for (const auto &p : res) {
-        std::cout << "{ bucket: " << p.mBucketIdx << ", data: " << p.mDataIdx << ", " << *p.mIter
-                  << "} \n";
-    }
+    const auto bp16 = BucketPolicy(groupDataTest::callables, {groupDataTest::vec1, groupDataTest::vec2}, false);
+    ASSERT_EQ(bp16.getInitialBucketCount(), 16);
+    const auto bp36 = BucketPolicy(groupDataTest::callables, {groupDataTest::vec3, groupDataTest::vec4}, false);
+    ASSERT_EQ(bp36.getInitialBucketCount(), 36);
+    const auto bp24 = BucketPolicy(groupDataTest::callables, {groupDataTest::vec1, groupDataTest::vec3}, false);
+    ASSERT_EQ(bp24.getInitialBucketCount(), 24);
 }
