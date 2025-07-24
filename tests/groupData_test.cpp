573b0705d87154a1f8f203d417588c6c0381549c
chefxxx <mateusz.mm100@gmail.com>
2025-07-24 13:32:31 +0200

    Add partition without overlfows test case

--- tests/groupData_test.cpp
+++ tests/groupData_test.cpp
@@ -36,22 +36,22 @@ const std::vector vec3{0.0, 0.5, 1.0, 1.5, 2.0};
 const std::vector vec4{0.0, 0.5, 1.0, 1.5, 2.0};
 
 /* Elements that are subject of binning */
-MyPoint p1{-0.1, 0.1};
-MyPoint p2{-0.1, -0.1};
-MyPoint p3{-0.1, 0.6};
-MyPoint p4{-0.1, 1.1};
-MyPoint p5{0.1, 0.1};
-MyPoint p6{0.1, -0.1};
-MyPoint p7{0.1, 0.6};
-MyPoint p8{0.1, 1.1};
-MyPoint p9{0.6, 0.1};
-MyPoint p10{0.6, -0.1};
-MyPoint p11{0.6, 0.6};
-MyPoint p12{0.6, 1.1};
-MyPoint p13{1.6, 0.1};
-MyPoint p14{1.6, -0.1};
-MyPoint p15{1.6, 0.6};
-MyPoint p16{1.6, 1.1};
+MyPoint p2{-0.1, -0.1}; // 0
+MyPoint p1{-0.1, 0.1};  // 1
+MyPoint p3{-0.1, 0.6};  // 2
+MyPoint p4{-0.1, 1.1};  // 3
+MyPoint p6{0.1, -0.1};  // 4
+MyPoint p5{0.1, 0.1};   // 5 non overflow in any dimension for vec1 and vec2 buckets
+MyPoint p7{0.1, 0.6};   // 6 non overflow in any dimension for vec1 and vec2 buckets
+MyPoint p8{0.1, 1.1};   // 7
+MyPoint p10{0.6, -0.1}; // 8
+MyPoint p9{0.6, 0.1};   // 9  non overflow in any dimension for vec1 and vec2 buckets
+MyPoint p11{0.6, 0.6};  // 10 non overflow in any dimension for vec1 and vec2 buckets
+MyPoint p12{0.6, 1.1};  // 11
+MyPoint p14{1.6, -0.1}; // 12
+MyPoint p13{1.6, 0.1};  // 13
+MyPoint p15{1.6, 0.6};  // 14
+MyPoint p16{1.6, 1.1};  // 15
 
 const std::vector points1{p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16};
 
@@ -83,5 +83,10 @@ TEST(GroupDataTest, bucketPartitionWithOverflows)
 
 TEST(GroupDataTest, bucketPartitionIgnoreOverflows)
 {
-
+    const auto indexes = groupData(groupDataTest::points1.begin(), groupDataTest::points1.end(), groupDataTest::bp16ignoreOverflows);
+    ASSERT_EQ(indexes.size(), 4);
+    const std::vector checkBuckets = {5, 6, 9, 10};
+    for (size_t i = 0; i < indexes.size(); i++) {
+        ASSERT_EQ(indexes[i].mBucketIdx, checkBuckets[i]);
+    }
 }
