5b77055ee94eb437ba80c295acfe92cb6c6565b1
MykhailoShamrai <mike.shamrai@gmail.com>
2025-09-08 16:17:25 +0200

    change to integral instead int

--- lib/Helpers.h
+++ lib/Helpers.h
@@ -12,7 +12,7 @@
 template <typename TBucketPolicy, typename TIter>
 concept is_bucket_policy = requires(TBucketPolicy policy, TIter iter) {
     { policy.getBucket(*iter) } -> std::same_as<int>;
-    { policy.getMaximalBucketCount() } -> std::same_as<int>;
+    { std::as_const(policy).getMaximalBucketCount() } -> std::integral;
     policy.getValues(*iter);
 };
 
