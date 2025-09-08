7ce2cbb5206ec1b0567e347197da6ce4722ab7d3
MykhailoShamrai <mike.shamrai@gmail.com>
2025-09-08 15:48:22 +0200

    change to fi

--- lib/Helpers.h
+++ lib/Helpers.h
@@ -28,7 +28,7 @@ struct BucketIdx {
     TIter mIter;
 };
 
-template <typename TIter, typename TBucketPolicy>
+template <std::forward_iterator TIter, typename TBucketPolicy>
     requires is_bucket_policy<TBucketPolicy, TIter>
 [[nodiscard]] auto groupData(
     TIter start, TIter end, TBucketPolicy bucketPolicy, const std::size_t minCatSize = 1
