48369ba733b707a579e0dcce3272bfc23ad0a603
chefxxx <mateusz.mm100@gmail.com>
2025-09-11 12:49:37 +0200

    Add IteratorType strcut, refactored CombinationsPolicyBase according to new changes

--- lib/Combinations.h
+++ lib/Combinations.h
@@ -7,21 +7,28 @@
 
 #include <tuple>
 
-template <typename... TData>
+template <typename TIter1, typename TIter2, typename... TRest>
+struct IteratorType {
+    using type = typename IteratorType<TIter1, TRest...>::type;
+};
+
+template <typename... TIter>
 struct CombinationsPolicyBase {
+    using CombinationsType = IteratorType<TIter...>::type;
     public:
-    CombinationsPolicyBase(TData &...data) {}
+    CombinationsPolicyBase(TIter... iterators) : mCurrentState() {}
 
     private:
-    std::tuple<typename TData::iterator...> mCurrentState;
+    std::tuple<CombinationsType> mCurrentState;
+    std::tuple<CombinationsType> mEndState;
 };
 
-template <typename... TData>
-struct FullCombinationsPolicy : CombinationsPolicyBase<TData...> {
+template <typename... TIter>
+struct FullCombinationsPolicy : CombinationsPolicyBase<TIter...> {
 };
 
-template <typename... TData>
-struct StrictlyUpperCombinationsPolicy : CombinationsPolicyBase<TData...> {
+template <typename... TIter>
+struct StrictlyUpperCombinationsPolicy : CombinationsPolicyBase<TIter...> {
 };
 
 #endif  // COMBINATIONS_H
