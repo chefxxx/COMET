b6375f584e5a885196b1633f8d256a67da148996
chefxxx <mateusz.mm100@gmail.com>
2025-09-21 18:48:14 +0200

    Changed operator[] of GroupedData (now copies)

--- lib/Helpers.h
+++ lib/Helpers.h
@@ -27,8 +27,8 @@ struct GroupedData {
     std::unordered_map<int, std::vector<TIter>> buckets;
     std::unordered_set<int> bucketsNumbers;
 
-    auto operator[](const int& bucketIdx) const { return buckets.at(bucketIdx); }
-    auto operator[](const IterType& iter) const { return buckets.at(*iter); }
+    auto operator[](int bucketIdx) const { return buckets.at(bucketIdx); }
+    auto operator[](IterType iter) const { return buckets.at(*iter); }
     [[nodiscard]] size_t size() const
     {
         if (bucketsNumbers.size() == buckets.size())
