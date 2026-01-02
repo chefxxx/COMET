e7b8133a3cad7b8af39f4f38d198606be2aae58e
MykhailoShamrai <mike.shamrai@gmail.com>
2026-01-02 16:51:21 +0100

    GroupedCombinations start of implementation

--- lib/GroupedCombinations.h
+++ lib/GroupedCombinations.h
@@ -5,4 +5,50 @@
 #ifndef COMET_GROUPEDCOMBINATIONS_H
 #define COMET_GROUPEDCOMBINATIONS_H
 
+#include "BucketSortView.h"
+
+template <typename TSource, typename TCallable>
+struct SourceWithCallable {
+    using source   = TSource;
+    using callable = TCallable;
+    source ProvidedSource;
+    callable ProvidedCallable;
+};
+
+template <typename TGrouping, typename TAssociated>
+concept ValidAssociated = AssociatedCallableResultIsConvertibleToGrouping<
+    typename TGrouping::source, typename TGrouping::callable, typename TAssociated::source,
+    typename TAssociated::callable>;
+
+template <typename TCombinationsProducer, typename TGrouping, typename... TAssociated>
+    requires(ValidAssociated<TGrouping, TAssociated> && ...)
+struct GroupedCombinations {
+    public:
+    explicit GroupedCombinations(
+        const TCombinationsProducer& combinations_producer, const TGrouping& grouping,
+        const TAssociated&... associated
+    )
+    {
+        m_combinationsProducer = combinations_producer;
+        m_views                = std::make_tuple(BucketSortView(
+            grouping.ProvidedSource, grouping.ProvidedCallable, associated.ProvidedSource,
+            associated.ProvidedCallable
+        )...);
+    }
+
+    private:
+    TCombinationsProducer m_combinationsProducer;
+    std::tuple<BucketSortView<
+        typename TGrouping::source, typename TGrouping::callable, typename TAssociated::source,
+        typename TAssociated::callable>...>
+        m_views;
+};
+
+template <
+    template <typename...> class TCombinationsPolicy, typename TBucketPolicy, typename TGrouping>
+
+auto makeGroupedCombinations()
+{
+}
+
 #endif  // COMET_GROUPEDCOMBINATIONS_H
