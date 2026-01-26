560f320494b350810d04ad3137bd7d378976dc7b
Mykhailo Shamrai <mykhailo.shamrai@horus.com.pl>
2026-01-26 18:45:16 +0100

    elo2

--- build/tests/COMET_test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/msh/CLionProjects/EventMixing2/build/tests/COMET_test[1]_tests.cmake")
-  include("/home/msh/CLionProjects/EventMixing2/build/tests/COMET_test[1]_tests.cmake")
-else()
-  add_test(COMET_test_NOT_BUILT COMET_test_NOT_BUILT)
-endif()
